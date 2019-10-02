ac9a201058aafd9703f9fca41a933e628fad1a8b
timkeane <tkeane@doitt.nyc.gov>
2019-10-02 19:29:55 -0400

    .

---
+++ import/import.sh
@@ -0,0 +1,7 @@
+IMPORT_PATH=$1
+SCRIPT_PATH="`dirname \"$0\"`"
+if [ "$IMPORT_PATH" == "" ]; then
+  echo "You must specify an import directory argument."
+  exit 1
+fi
+./$SCRIPT_PATH/workspaces.sh $IMPORT_PATH
