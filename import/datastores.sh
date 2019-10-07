a08a624cc49e33df6eb778947b62e2d5777931e9
timkeane <tkeane@doitt.nyc.gov>
2019-10-07 16:46:51 -0400

    not sure why put results in redis error

--- import/datastores.sh
+++ import/datastores.sh
@@ -5,11 +5,22 @@ echo
 echo "Configuring datastores for workspace '$wsName' ..."
 datastores=(`ls $IMPORT_PATH/namespaces/$wsName/datastores`)
 for dsName in ${datastores[*]}; do
-  sleep 1
-  echo
-  echo "Configuring datastore '$dsName' ..."
-  echo "curl -X POST $GS_REST/workspaces/$wsName/datastores.json -d \"@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/datastore.json\" -H \"Content-Type: application/json\""
-  curl -X POST $GS_REST/workspaces/$wsName/datastores.json -d "@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/datastore.json" -H "Content-Type: application/json"
-  echo
-  ./$SCRIPT_PATH/featuretypes.sh $IMPORT_PATH $wsName $dsName
+  ENDPOINT="workspaces/$wsName/datastores.json"
+  METHOD="POST"
+  while [ true ]; do
+    sleep 1
+    echo
+    echo "Configuring datastore '$dsName' ..."
+    echo "curl -X $METHOD $GS_REST/$ENDPOINT -d \"@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/datastore.json\" -H \"Content-Type: application/json\""
+    status=$(curl -X $METHOD $GS_REST/$ENDPOINT -d "@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/datastore.json" -H "Content-Type: application/json")
+    echo $status
+    if [[ $status == "200" ]]; then
+      ./$SCRIPT_PATH/featuretypes.sh $IMPORT_PATH $wsName $dsName
+      break
+    else
+      ENDPOINT="workspaces/$wsName/datastores/$dsName.json"
+      METHOD="PUT"
+    fi
+    echo
+  done
 done
--- import/featuretypes.sh
+++ import/featuretypes.sh
@@ -5,10 +5,20 @@ echo
 echo "Configuring featuretypes for datastore '$dsName' ..."
 featuretypes=(`ls $IMPORT_PATH/namespaces/$wsName/datastores/$dsName/featuretypes`)
 for ftName in ${featuretypes[*]}; do
-  sleep 1
-  echo
-  echo "Configuring featuretype '${ftName/\.json/}' ..."
-  echo "curl -X POST $GS_REST/workspaces/$wsName/datastores/$dsName/featuretypes.json -d \"@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/featuretypes/$ftName\" -H \"Content-Type: application/json\""
-  curl -X POST $GS_REST/workspaces/$wsName/datastores/$dsName/featuretypes.json -d "@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/featuretypes/$ftName" -H "Content-Type: application/json"
-  echo
+  ENDPOINT="workspaces/$wsName/datastores/$dsName/featuretypes.json"
+  METHOD="POST"
+  while [ true ]; do
+    sleep 1
+    echo
+    echo "Configuring featuretype '${ftName/\.json/}' ..."
+    echo "curl -X $METHOD $GS_REST/$ENDPOINT -d \"@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/featuretypes/$ftName\" -H \"Content-Type: application/json\""
+    status=(curl -X $METHOD $GS_REST/$ENDPOINT -d "@${IMPORT_PATH}/namespaces/$wsName/datastores/$dsName/featuretypes/$ftName" -H "Content-Type: application/json")
+    if [[ $status == "200" ]]; then
+      break
+    else
+      ENDPOINT="workspaces/$wsName/datastores/$dsName/featuretypes/$ftName"
+      METHOD="PUT"
+    fi
+    echo
+  done
 done
