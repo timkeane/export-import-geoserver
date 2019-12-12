03234cc74fef3de026d7ce492437d1771511f6c1
timkeane <t.d.keane@gmail.com>
2019-12-12 11:29:10 -0500

    .

--- import/gwc.sh
+++ import/gwc.sh
@@ -6,13 +6,13 @@ echo
 echo "Configuring gwc layers ..."
 layers=(`ls $IMPORT_PATH/gwc/layers`)
 for lName in ${layers[*]}; do
-  ENDPOINT="layers.json"
+  ENDPOINT="layers"
   METHOD="PUT"
   sleep 1
   echo
   echo "Configuring layer '$lName' ..."
-  echo "curl -s -o /dev/null -w '%{http_code}' -X $METHOD $GWC_REST/$ENDPOINT -d \"@${IMPORT_PATH}/gwc/layers/$lName/layer.json\" -H \"Content-Type: application/json\""
-  status=$(curl -s -o /dev/null -w '%{http_code}' -X $METHOD $GWC_REST/$ENDPOINT -d "@${IMPORT_PATH}/gwc/layers/$lName/layer.json" -H "Content-Type: application/json")
+  echo "curl -s -o /dev/null -w '%{http_code}' -X $METHOD $GWC_REST/$ENDPOINT/$lName.json -d \"@${IMPORT_PATH}/gwc/layers/$lName/layer.json\" -H \"Content-Type: application/json\""
+  status=$(curl -s -o /dev/null -w '%{http_code}' -X $METHOD $GWC_REST/$ENDPOINT/$lName.json -d "@${IMPORT_PATH}/gwc/layers/$lName/layer.json" -H "Content-Type: application/json")
   echo "HTTP Status $status"
   echo
 done
